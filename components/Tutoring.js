5b45ad4e74cedadb0ccfd1f7343961492f524528
maxStrzelecki <140175269+maxStrzelecki@users.noreply.github.com>
2025-09-13 14:08:06 +0200

    Update Tutoring.js - added links and strong in simulations

--- components/Tutoring.js
+++ components/Tutoring.js
@@ -70,12 +70,14 @@ export default function Tutoring() {
 								<SVG className="size-7 text-primary" />
 								<div className="w-full">
 									<strong>Równania i wzory w LaTeX</strong> – wszystkie równania, przekształcenia i wzory zapisujemy na żywo w systemie LaTeX, dzięki czemu są czytelne, eleganckie i gotowe do natychmiastowego eksportu do pliku PDF.
+									<a href="https://youtu.be/Lx7aadHBYs4">Tu znajdziesz video jak to u nasz działa</a>
 								</div>
 							</div>
 							<div className="text-gray-300 flex gap-3 items-start">
 								<SVG className="size-7 text-primary" />
 								<div className="w-full">
-									Interaktywne symulacje fizyczne – nasze lub sprawdzone symulacje z Internetu pomagają zrozumieć trudne zjawiska fizyczne i rozwijają intuicję.
+									<strong>Interaktywne symulacje fizyczne</strong> – nasze lub sprawdzone symulacje z Internetu pomagają zrozumieć trudne zjawiska fizyczne i rozwijają intuicję.
+									<a href="https://youtu.be/LTyorAPp9Ps">A tu pokazujemy Ci przykładowe symulacje</a>
 								</div>
 							</div>
 							<div className="text-gray-300 flex gap-3 items-start">
