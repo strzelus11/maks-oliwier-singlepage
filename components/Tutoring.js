bec16b2aa0f41536c66608584d99f04d7ff5d0d6
maxStrzelecki <140175269+maxStrzelecki@users.noreply.github.com>
2025-09-13 14:26:36 +0200

    Update Tutoring.js - update links

--- components/Tutoring.js
+++ components/Tutoring.js
@@ -69,15 +69,15 @@ export default function Tutoring() {
 							<div className="text-gray-300 flex gap-3 items-start">
 								<SVG className="size-7 text-primary" />
 								<div className="w-full">
-									<strong>Równania i wzory w LaTeX</strong> – wszystkie równania, przekształcenia i wzory zapisujemy na żywo w systemie LaTeX, dzięki czemu są czytelne, eleganckie i gotowe do natychmiastowego eksportu do pliku PDF.
-									<a href="https://youtu.be/Lx7aadHBYs4">Tu znajdziesz video jak to u nasz działa</a>
+									<strong>Równania i wzory w LaTeX</strong> – wszystkie równania, przekształcenia i wzory zapisujemy na żywo w systemie LaTeX, dzięki czemu są czytelne, eleganckie i gotowe do natychmiastowego eksportu do pliku PDF.<br>
+									<a className="text-primary", href="https://youtu.be/Lx7aadHBYs4">Tu znajdziesz video jak to u nasz działa</a>
 								</div>
 							</div>
 							<div className="text-gray-300 flex gap-3 items-start">
 								<SVG className="size-7 text-primary" />
 								<div className="w-full">
-									<strong>Interaktywne symulacje fizyczne</strong> – nasze lub sprawdzone symulacje z Internetu pomagają zrozumieć trudne zjawiska fizyczne i rozwijają intuicję.
-									<a href="https://youtu.be/LTyorAPp9Ps">A tu pokazujemy Ci przykładowe symulacje</a>
+									<strong>Interaktywne symulacje fizyczne</strong> – nasze lub sprawdzone symulacje z Internetu pomagają zrozumieć trudne zjawiska fizyczne i rozwijają intuicję.<br>
+									<a className="text-primary", href="https://youtu.be/LTyorAPp9Ps">A tu pokazujemy Ci przykładowe symulacje</a>
 								</div>
 							</div>
 							<div className="text-gray-300 flex gap-3 items-start">
