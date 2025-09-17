851d7dfb19462099edcb1fcf7ebff88ada818f6e
maxStrzelecki <140175269+maxStrzelecki@users.noreply.github.com>
2025-09-17 19:14:28 +0200

    Create thank-you.js

---
+++ pages/thank-you.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ThankYou = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Dziękujemy za zapisanie się!</h1>
+      <p>Skontaktujemy się z Tobą wkrótce.</p>
+    </div>
+  );
+};
+
+export default ThankYou;
