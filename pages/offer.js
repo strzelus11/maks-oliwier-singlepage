8155799de8022ced02279aea9e84f124192b9693
maxStrzelecki <140175269+maxStrzelecki@users.noreply.github.com>
2025-09-18 21:12:21 +0200

    Update offer.js

--- pages/offer.js
+++ pages/offer.js
@@ -1,9 +1,25 @@
-import Offer from "@/components/Offer";
+import Head from "next/head";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Choose from "@/components/Choose";
 
 export default function ContactPage() {
   return (
-    <main className="flex items-center justify-center h-screen bg-gray-900">
-      <Offer className="screen" />
-    </main>
+    <>
+
+      <div className="flex flex-col min-h-screen bg-gray-900">
+        {/* Header */}
+        <Header />
+
+        {/* Główna treść */}
+        <div className="screen">
+          <Choose />
+        </div>
+
+        {/* Footer */}
+        <Footer />
+      </div>
+    </>
   );
 }
+
