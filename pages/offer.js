c711887cc381da29ed0fa443f19954982dc508d0
maxStrzelecki <140175269+maxStrzelecki@users.noreply.github.com>
2025-09-18 20:58:45 +0200

    Create offer.js

---
+++ pages/offer.js
@@ -0,0 +1,9 @@
+import Offer from "@/components/Offer";
+
+export default function ContactPage() {
+  return (
+    <main className="flex items-center justify-center h-screen bg-gray-900">
+      <Offer className="screen" />
+    </main>
+  );
+}
